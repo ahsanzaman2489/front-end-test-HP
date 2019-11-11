66daf503a76d44ccb6fedbba0d9135199a36ed56
ahsan <ahsan.zaman@eyewa.com>
2019-11-11 18:42:47 +0400

    Options merge to create the layout

--- src/scripts/classes/Main.js
+++ src/scripts/classes/Main.js
@@ -1,7 +1,14 @@
 import {makeRequest} from '../service/';
 
 class Main {
-  static defaultOptions() {
+  #options;
+  #data;
+
+  constructor(options) {
+    this.mergeOptions(options);
+  }
+
+  defaultOptions() {
     return { //Defaults
       sorting: {
         stars: false,
@@ -9,20 +16,38 @@ class Main {
       },
       count: 5,
       reviews: false,
-      pagination: 'pagination',
+      pagination: 'lazy',
       element: '.hotel-booking-wrapper'
     }
   }
 
+  mergeOptions(options) {
+    this.#options = Object.assign({}, this.defaultOptions(), options);
+    this.getData();
+  }
+
   async getData() {
     const response = await makeRequest('http://fake-hotel-api.herokuapp.com/api/hotels');
-    console.log(response)
-    return response;
+    this.#data = response.data;
+
+    this.createLayout();
   };
 
-  constructor(options) {
-    this.getData()
+  createLayout() {
+    const {element} = this.#options;
+    const $element = document.getElementById(element);
+    $element.innerHTML = $element.innerHTML + `<h1>Ahsan</h1>`;
   }
+
+
+  // renderData() {
+  //   const {count} = this.#options;
+  //   console.log(count)
+  //   for (let index = 0; index < count; index = index + 1) {
+  //     console.log('hellowolrd')
+  //   }
+  // }
+
 
 }
 
